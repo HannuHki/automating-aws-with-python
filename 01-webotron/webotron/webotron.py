ec18e9b48c83a48e1b6e342bf0dd78616ac12837
HannuHki <hannu_hautamaki@hotmail.com>
2020-05-14 13:27:12 +0300

    Clean up code

--- 01-webotron/webotron/webotron.py
+++ 01-webotron/webotron/webotron.py
@@ -1,3 +1,17 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""Webotron: Deploy websites with aws.
+
+Webotron automates the process of deploying static websites to AWS.
+- Configure AWS S3 buckets
+  - Create them
+  - Set them up for static website hosting
+  - Deploy local files to them
+- Configure DNS with AWS Route 53
+- Configure a Content Delivery Network and SSL with AWS CloudFront
+"""
+
 import boto3
 import click
 from botocore.exceptions import ClientError
@@ -7,16 +21,19 @@ import mimetypes
 session = boto3.Session(profile_name='pythonAutomation')
 s3 = session.resource('s3')
 
+
 @click.group()
 def cli():
-    "Webotron deploys websites to AWS"
+    """Webotron deploys websites to AWS"""
     pass
+
 
 @cli.command('list-buckets')
 def list_buckets():
-    "List all s3 buckets"
+    """List all s3 buckets"""
     for bucket in s3.buckets.all():
         print(bucket)
+
 
 @cli.command('list-bucket-objects')
 @click.argument('bucket')
@@ -28,7 +45,7 @@ def list_bucket_objects(bucket):
 @cli.command('setup-bucket')
 @click.argument('bucket')
 def setup_bucket(bucket):
-    "Create and configure S3 bucket"
+    """Create and configure S3 bucket"""
     S3_bucket = None
     try:
         s3_bucket = s3.create_bucket(
@@ -76,6 +93,7 @@ def setup_bucket(bucket):
     return
 
 def upload_file(s3_bucket, path, key):
+    """Upload path to s3_bucket at key."""
     s3_bucket.upload_file(
         path,
         key,
